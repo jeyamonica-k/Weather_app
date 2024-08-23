e821c0aa3b9369d37bb793c060e2fe83a9e5c48a
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2024-08-23 19:08:32 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+  return(<div>
+    <h1 className="text-4xl text-red-600 animate-pulse m-3">Weather Report!</h1>
+    <p className=" text-black text-xl p-1 m-2">I can give you a Weather report of your city</p>
+  </div>
+  )
+}
+
+export default Header
---
+++ src/components/InputComp.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+import axios from "axios"
+
+function InputComp(){
+
+const[userInput,setuserInput]=useState("")
+const[weather,setweather]=useState("")
+const[tempraure,settemprature]=useState("")
+const[description,setdescription]=useState("")
+
+function handleInput(evt){
+
+  setuserInput(evt.target.value)
+
+}
+
+function handleCheck(){
+  const cityWeather = axios(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=31982986afa32912f8411593f3e53503`)
+  cityWeather.then(
+    function(success){
+      setweather(success.data.weather[0].main)
+      settemprature(success.data.main.temp)
+      setdescription(success.data.weather[0].description)
+    }
+  ).catch(
+    function(){
+      console.log("failed")
+    }
+  )
+
+}
+  return(<div>
+    
+    <input 
+    className="p-2 border rounded-md border-black bg-transparent m-2 placeholder-red-600"
+    placeholder="Enter your city name"
+    onChange={handleInput}></input>
+    <button className="bg-red-500  text-black p-2 border-none rounded-md" onClick={handleCheck}>Get Report</button>
+    <p className="text-xl text-black p-1">Weather: {weather}</p>
+    <p className="text-xl text-black p-1">Teamprature: {tempraure}</p>
+    <p className="text-xl text-black p-1">Description: {description}</p>
+  </div>)
+}
+
+export default InputComp
